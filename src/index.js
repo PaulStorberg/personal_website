234c7641774a3a2a6c125ca3909ba64549debcf2
Paul Storberg <Paul@storberg.net>
2019-04-10 14:04:16 -0400

    substantial styling to layout

--- src/index.js
+++ src/index.js
@@ -6,13 +6,34 @@ class Body extends React.Component {
   render() {
 
     return (
-      <div>
-        <div className="background-image">
-        </div>
+      <div className="background-image">
+        <div className="scroll-container">
 
-        Paul J Storberg
+          <div className="overlay-container jumbotron">
+            <div className="jumbotron-name"> Paul J Storberg </div>
+            <div className="jumbotron-title"> Front-end Web Developer </div>
+          </div>
 
-        <div className="footer">
+          <div className="overlay-container portfolio-container">
+            <div className="">
+            </div>
+
+            <div className="">
+            </div>
+          </div>
+
+          <div className="overlay-container contact-container">
+          </div>
+
+          <div className="overlay-container links-container">
+            <div className="link">
+              <a className="linkedin" href="https://www.linkedin.com/in/PaulStorberg">LinkedIn</a>
+            </div>
+            <div className="link">
+              <a className="github" href="https://github.com/PaulStorberg">GitHub</a>
+            </div>
+          </div>
+
         </div>
       </div>
     );
