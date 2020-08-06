54bb5401854a488d43abfdae7dff729412a58f32
Paul Storberg <paul@storberg.net>
2020-08-06 11:54:50 -0400

    updated layout and style

--- src/index.js
+++ src/index.js
@@ -1,72 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import portfolio from "./portfolio"
 import './index.css';
 
 class Body extends React.Component {
+  displayContactForm() {
+    var form = document.getElementById("contactForm");
+    var jumbotron = document.getElementById("jumbotronSlide");
+    form.classList.toggle("active-form");
+    jumbotron.classList.toggle("jumbotron-slide");
+  }
+
   render() {
-
-    const portfolioArray = this.props.data;
-
-    const portfolioCards = portfolioArray.map(card => {
-      return (
-        <div className="card" key={card.title}>
-          <img className="card-img-top" src={card.src} alt="Image Not Working at This Time" />
-          <div className="card-body">
-            <h5 className="card-title"><a className="portfolio-link link" href={card.href}>{card.title}</a></h5>
-            <p className="card-text">{card.text}</p>
-          </div>
-        </div>
-      )
-    });
 
     return (
       <div className="background-image">
-        <div className="scroll-container">
+        <div className="container no-gutter scroll-container">
 
-          <div className="overlay-container top-links-container">
-            <div className="link-top">
-              <a className="linkedin-top link" href="https://www.linkedin.com/in/PaulStorberg"><i className="fab fa-linkedin"></i></a>
+          <div className="row no-gutter top-links-container">
+            <div className="col col-xs-3 no-gutter link-top">
+              <a className="linkedin-top link" href="https://www.linkedin.com/in/PaulStorberg">LinkedIn</a>
             </div>
-            <div className="link-top">
-              <a className="github-top link" href="https://github.com/PaulStorberg"><i className="fab fa-github-square"></i></a>
+            
+            <div className="col col-xs-3 no-gutter link-top">
+              <a className="github-top link" href="https://www.upwork.com/o/profiles/users/_~010d7a419385669325/">UpWork</a>
+            </div>
+
+            <div className="col col-xs-3 no-gutter link-top">
+              <a className="github-top link" href="https://github.com/PaulStorberg">GitHub</a>
+            </div>
+
+            <div className="col col-xs-3 no-gutter link-top">
+              <a className="github-top link" onClick={this.displayContactForm}>Contact</a>
             </div>
           </div>
 
-          <div className="overlay-container jumbotron">
+          <div className="container no-gutter jumbotron jumbotron-slide" id="jumbotronSlide">
             <div className="jumbotron-name"> Paul J Storberg </div>
-            <div className="jumbotron-title"> Software Engineer </div>
+            <div className="jumbotron-title"> Full Stack Developer </div>
           </div>
 
-          <div className="overlay-container portfolio-container container">
-            <div className="section-title">Portfolio</div>
-            <div className="card-columns">
-              {portfolioCards}
-            </div>
-          </div>
-
-          <div className="overlay-container contact-container">
+          <div className="contact-container active-form" id="contactForm">
             <form className="form-layout" action="https://formspree.io/paul@storberg.net" method="POST" >
-              <div className="form-title">Project inquiries</div>
+              <div className="form-title">Project Inquiries</div>
               <input className="form-text" type="text" name="name" placeholder="Name" />
               <input className="form-text" type="text" name="email" placeholder="Email" />
-              <input className="form-text" type="text" name="budget" placeholder="Budget" />
               <textarea className="form-text" type="text" rows="3" name="outline" placeholder="Project Outline" />
-              <button className="form-submit" action="submit">submit</button>
+              <button className="form-submit" action="submit">SUBMIT</button>
             </form>
-          </div>
-
-          <div className="overlay-container links-container">
-            <div className="link-bottom">
-              <a className="linkedin-bottom link" href="https://www.linkedin.com/in/PaulStorberg">LinkedIn</a>
-            </div>
-            <div className="link-bottom">
-              <a className="upwork-bottom link" href="https://www.upwork.com/o/profiles/users/_~010d7a419385669325/">UpWork</a>
-            </div>
-            <div className="link-bottom">
-              <a className="github-bottom link" href="https://github.com/PaulStorberg">GitHub</a>
-            </div>
           </div>
 
         </div>
@@ -78,6 +59,6 @@ class Body extends React.Component {
 // =============================================================================
 
 ReactDOM.render(
-  <Body data={portfolio} />,
+  <Body />,
   document.getElementById('root')
 );
