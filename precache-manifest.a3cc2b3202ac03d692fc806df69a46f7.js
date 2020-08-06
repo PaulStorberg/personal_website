4b07d2a8c5270ab60b4212dd618263820aca3126
Paul Storberg <paul@storberg.net>
2020-08-06 14:34:04 -0400

    Updates

---
+++ precache-manifest.a3cc2b3202ac03d692fc806df69a46f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ba4442a33b15ab374d7",
+    "url": "/static/css/main.ecfce3b5.chunk.css"
+  },
+  {
+    "revision": "4ba4442a33b15ab374d7",
+    "url": "/static/js/main.5501a3cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82d9a690305a6090c325",
+    "url": "/static/css/2.dc631d02.chunk.css"
+  },
+  {
+    "revision": "82d9a690305a6090c325",
+    "url": "/static/js/2.f498ee26.chunk.js"
+  },
+  {
+    "revision": "75576ecc6473f8f82d60bbcbaff38165",
+    "url": "/index.html"
+  }
+];
